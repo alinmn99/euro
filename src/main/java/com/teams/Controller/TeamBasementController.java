b3d0bb2ceb091de501518b6acabe6c8c9295642d
MANOLE ALIN IONUT <cs.alin.krom@gmail.com>
2021-08-18 17:11:13 +0300

    18 august

--- src/main/java/com/teams/Controller/TeamBasementController.java
+++ src/main/java/com/teams/Controller/TeamBasementController.java
@@ -1,24 +1,28 @@
 package com.teams.Controller;
 
+import com.teams.Entities.Player;
+import com.teams.Repository.PlayerRepository;
 import com.teams.Repository.TeamBasementRepository;
 import com.teams.Entities.TeamBasement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/teamBasement")
 public class TeamBasementController {
 
     @Autowired
     TeamBasementRepository teamBasementRepository;
 
-    @GetMapping("/teambasement")
+    @GetMapping("/list")
     public long shotTeamBasement (Model model) {
-        List<TeamBasement>teamBasementList = (List<TeamBasement>) teamBasementRepository.findAll();
-        model.addAttribute("teamBasement",teamBasementList);
+        List<TeamBasement> teamBasement = (List<TeamBasement>) teamBasementRepository.findAll();
+        model.addAttribute("teamBasement",teamBasement);
         return Long.parseLong("teamBasement");
     }
 }
